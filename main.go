95648fd11b29a8bd6213e04334a5135c8089ca4c
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-13 11:56:42 -0300

    Connecting to the Postgresql db

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func dbConnection() *sql.DB {
+
+	// Setting the connections of the DataBase
+	connection := "user=postgres dbname=store_project password=zeu$@2022 host=localhost sslmode=disable"
+
+	// Open opens a database specified by its database driver name and a driver-specific data source name
+	db, err := sql.Open("postgres", connection)
+
+	// Catching the errors
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
 
 // The product struct
 type Product struct {
+	Id          int
 	Name        string
 	Description string
 	Price       float64
@@ -25,12 +45,36 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 
-	// This slice will be set directly in the web server
-	produtos := []Product{
-		{Name: "Iphone 8s", Description: "64GB", Price: 450.00, Quantity: 1},
-		{"PS4", "1T", 500.00, 5},
-		{"XBOX ONE", "500GB", 350.00, 4},
+	db := dbConnection()
+
+	allProducts, err := db.Query("select * from products")
+
+	if err != nil {
+		panic(err.Error())
 	}
 
-	tmp.ExecuteTemplate(w, "Index", produtos)
+	p := Product{}
+	products := []Product{}
+
+	// With the function Next, all the columns and lines from the query will be checked
+	for allProducts.Next() {
+		var id, quantity int
+		var name, description string
+		var price float64
+
+		err = allProducts.Scan(&id, &name, &description, &price, &quantity)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Name = name
+		p.Description = description
+		p.Price = price
+		p.Quantity = quantity
+
+		products = append(products, p)
+	}
+
+	tmp.ExecuteTemplate(w, "Index", products)
+	defer db.Close()
 }
