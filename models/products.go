e9889bca319fbe9cd7743b896996d82448d7b459
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-14 11:11:37 -0300

    Searching in the db for the products to update

--- models/products.go
+++ models/products.go
@@ -16,7 +16,7 @@ type Product struct {
 func SearchAllProducts() []Product {
 	db := db.DbConnection()
 
-	allProducts, err := db.Query("select * from products")
+	allProducts, err := db.Query("select * from products order by id asc")
 
 	if err != nil {
 		panic(err.Error())
@@ -38,6 +38,7 @@ func SearchAllProducts() []Product {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Name = name
 		p.Description = description
 		p.Price = price
@@ -63,4 +64,64 @@ func CreateNewProduct(name, description string, price float64, quantity int) {
 
 	dbInsertion.Exec(name, description, price, quantity)
 	defer db.Close()
+}
+
+func DeleteProduct(id string) {
+
+	db := db.DbConnection()
+	deleteProduct, err := db.Prepare("delete from products where id=$1")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deleteProduct.Exec(id)
+	defer db.Close()
+}
+
+func EditProduct(id string) Product {
+
+	db := db.DbConnection()
+
+	dbProduct, err := db.Query("select * from products where id=$1", id)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	productToUpdate := Product{}
+
+	for dbProduct.Next() {
+		var id, quantity int
+		var name, description string
+		var price float64
+
+		err = dbProduct.Scan(&id, &name, &description, &price, &quantity)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		productToUpdate.Id = id
+		productToUpdate.Name = name
+		productToUpdate.Description = description
+		productToUpdate.Price = price
+		productToUpdate.Quantity = quantity
+	}
+
+	defer db.Close()
+	return productToUpdate
+}
+
+func UpdateProduct(id int, name, description string, price float64, quantity int) {
+
+	db := db.DbConnection()
+
+	updateProduct, err := db.Prepare("update products set nome=$1, description=$2, price=$3, quantity=$4 where id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+	updateProduct.Exec(name, description, price, quantity, id)
+
+	defer db.Close()
+
 }
