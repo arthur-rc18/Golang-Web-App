93dabeb6e80e90e83830909aa4d3660932e9199b
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-14 11:12:22 -0300

    Setting the route to the update request

--- routes/routes.go
+++ routes/routes.go
@@ -11,4 +11,7 @@ func LoadRoutes() {
 	http.HandleFunc("/", controller.Index)
 	http.HandleFunc("/new", controller.New)
 	http.HandleFunc("/insert", controller.Insert)
+	http.HandleFunc("/delete", controller.Delete)
+	http.HandleFunc("/edit", controller.Edit)
+	http.HandleFunc("/update", controller.Update)
 }
